73c27844039a39f09e4b0111ad4bb0a5c400e715
Jean Francisquini <53776846+jeanfrancisquini@users.noreply.github.com>
2023-11-16 22:43:19 -0300

    BBBBBB

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const router = require('./src/routes/routes');
+//const router = require('./src/routes/routes');
 
 const app = express();
 
