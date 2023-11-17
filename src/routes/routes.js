15c0b78eb53e02dda1f636d0d40bb306c3e739ec
Jean Francisquini <53776846+jeanfrancisquini@users.noreply.github.com>
2023-11-16 22:56:16 -0300

    TTTTT

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,4 @@
-//const connection = require('../database/connection');
+const connection = require('../database/connection');
 const express = require('express');
 const router = express.Router();
 //const DefUserTypeController = require('../controllers/Def/DefUserTypeController');
