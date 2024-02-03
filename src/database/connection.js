dd9f9adb235778b062d290767fb5c83e12241a02
Jean Francisquini <53776846+jeanfrancisquini@users.noreply.github.com>
2024-02-03 00:53:13 -0300

    AJuste database

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,11 +1,22 @@
 
+// const knex = require('knex')({
+//   client: 'mssql',
+//   connection: {
+//     server: "cdpd-db.c1qhsihaqmhn.us-east-2.rds.amazonaws.com",
+//     user: "admin",
+//     password: "x5Cjy4fPe5IL5fh",
+//     database: "cdpd-database"    
+//   }
+// });
+
 const knex = require('knex')({
-  client: 'mssql',
+  client: 'mysql',
   connection: {
-    server: "cdpd-db.c1qhsihaqmhn.us-east-2.rds.amazonaws.com",
-    user: "admin",
-    password: "x5Cjy4fPe5IL5fh",
-    database: "cdpd-database"    
+    host: "br1122.hostgator.com.br",
+    port: 3306,
+    user: "digi7412_jean",
+    password: "AliciaMiguel@@11282526",
+    database: "digi7412_cdpdDB"        
   }
 });
 
