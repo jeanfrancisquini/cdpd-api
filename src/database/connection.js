1e3762a1425597b5b08e0289e96436c8cf01699c
Jean Francisquini <53776846+jeanfrancisquini@users.noreply.github.com>
2024-02-04 23:39:07 -0300

    retorno AWS

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,23 +1,23 @@
 
-// const knex = require('knex')({
-//   client: 'mssql',
-//   connection: {
-//     server: "cdpd-db.c1qhsihaqmhn.us-east-2.rds.amazonaws.com",
-//     user: "admin",
-//     password: "x5Cjy4fPe5IL5fh",
-//     database: "cdpd-database"    
-//   }
-// });
-
 const knex = require('knex')({
-  client: 'mysql',
+  client: 'mssql',
   connection: {
-    host: "br1122.hostgator.com.br",
-    port: 3306,
-    user: "digi7412_jean",
-    password: "AliciaMiguel@@11282526",
-    database: "digi7412_cdpdDB"        
+    server: "cdpd-db.c1qhsihaqmhn.us-east-2.rds.amazonaws.com",
+    user: "admin",
+    password: "x5Cjy4fPe5IL5fh",
+    database: "cdpd-database"    
   }
 });
+
+// const knex = require('knex')({
+//   client: 'mysql',
+//   connection: {
+//     host: "br1122.hostgator.com.br",
+//     port: 3306,
+//     user: "digi7412_jean",
+//     password: "AliciaMiguel@@11282526",
+//     database: "digi7412_cdpdDB"        
+//   }
+// });
 
 module.exports = knex
