73faeb605e2ab2d2a63722c9dca19afb2df1d86e
Jean Francisquini <53776846+jeanfrancisquini@users.noreply.github.com>
2024-07-29 22:46:10 -0300

    Update ReleaseController.js

--- src/controllers/ReleaseController.js
+++ src/controllers/ReleaseController.js
@@ -28,30 +28,44 @@ class ReleaseController{
             activityId 
           } = request.body;
 
-        database.select("id",
-                        "id_usuario as userId",
-                        "valor as value",
-                        "data_pagamento as paymentDate",
-                        "data_validade as nextPayment",
-                        "id_tipo_pagamento as idTypePayment",
-                        "id_atividade as idActivity",
-                        "obs"
-                        )
-                    .modify(function(queryBuilder) {
-                
-                        if (userId != null) {
-                            queryBuilder.where({"id_usuario": userId ?? null})
-                        }
-                        if (activityId != null) {
-                            queryBuilder.where({"id_atividade": activityId ?? null})
-                        }                    
-                    })
-                .table("lancamento").then(data => {
-                    console.log(data);
-                    response.json(data);
-                }).catch(error => {
-                    console.log(error);
+
+        database.raw('exec stp_busca_agendamento ?,?,?,?',[userId ?? null,activityId?? null])
+          .then(async data => {
+                var resultsPromise = data.map(async (obj) => {
+
+                    //obj.time = moment(obj.time).format('HH:mm:ss')
+                    return obj
                 })
+
+                response.json(await Promise.all(resultsPromise)); 
+          }).catch(error => {
+              console.log(error);
+          })
+
+        // database.select("id",
+        //                 "id_usuario as userId",
+        //                 "valor as value",
+        //                 "data_pagamento as paymentDate",
+        //                 "data_validade as nextPayment",
+        //                 "id_tipo_pagamento as idTypePayment",
+        //                 "id_atividade as idActivity",
+        //                 "obs"
+        //                 )
+        //             .modify(function(queryBuilder) {
+                
+        //                 if (userId != null) {
+        //                     queryBuilder.where({"id_usuario": userId ?? null})
+        //                 }
+        //                 if (activityId != null) {
+        //                     queryBuilder.where({"id_atividade": activityId ?? null})
+        //                 }                    
+        //             })
+        //         .table("lancamento").then(data => {
+        //             console.log(data);
+        //             response.json(data);
+        //         }).catch(error => {
+        //             console.log(error);
+        //         })
     }
 
 }
